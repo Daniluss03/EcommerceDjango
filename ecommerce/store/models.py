ee36523d2110824b385816bbe7fa98bba5469280
Danilo <jhondanilo3@gmail.com>
2025-01-08 19:40:23 -0500

    First templates  base and store also add fk in the models

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -14,6 +14,7 @@ class Category(models.Model):
     
 
 class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE,null=True)
     title=models.CharField(max_length=250)
     brand=models.CharField(max_length=250,default='un-branded')
     description=models.TextField(max_length=250,blank=True)
